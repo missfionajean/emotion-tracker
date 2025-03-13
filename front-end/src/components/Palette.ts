9ac3d6f5fe1f09de0e27a049eae4f15fd5f77754
missfionajean <missfionajean@gmail.com>
2025-03-13 18:49:54 -0400

    added color palette to repo and imported into home as test

---
+++ front-end/src/components/Palette.ts
@@ -0,0 +1,20 @@
+const palette = {
+    depressed: "#C180FF",
+    pessimistic: "#DF80FE",
+    sad: "#FE80FE",
+    upset: "#FE80BF",
+    angry: "#FE8081",
+    irritable: "#FFBF81",
+    tense: "#FFFF80",
+    nervous: "#E2FF7F",
+    excited: "#81FF81",
+    optimistic: "#81FFBF",
+    happy: "#7FFFDE",
+    content: "#80DFFF",
+    relaxed: "#7FBFFF",
+    calm: "#819FFF",
+    bored: "#807FFE",
+    gloomy: "#A080FF"
+}
+
+export default palette
